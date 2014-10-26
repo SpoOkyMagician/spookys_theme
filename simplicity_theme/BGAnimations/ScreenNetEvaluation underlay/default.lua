d79ab0dcab3d28da0f3ba52d020bb3a49ff3f0c3
Jeremy Levegood <levegoods@twc.com>
2014-10-25 20:56:03 -0400

    Minor change to online evaluation
    I am just gonna use p1 stats. It works for now. It will not show others
    scores because their broken right now... Nothing I can do about that
    sorry.

--- simplicity_theme/BGAnimations/ScreenNetEvaluation underlay/default.lua
+++ simplicity_theme/BGAnimations/ScreenNetEvaluation underlay/default.lua
@@ -7,14 +7,67 @@ local t = Def.ActorFrame{
 	common_text("StepMania Online Evaluation"),
 	-- GLOBAL dark quad
 	grid_t,
-	-- results MP text
+	-- results P1 text
 	LoadFont("SpoOky")..{
-		Text="Results MP";
+		Text="Results P1";
 		InitCommand=cmd(x,SCREEN_LEFT+8;y,SCREEN_BOTTOM/2;diffuse,color(theme_color);zoom,0.75;align,0,0.5;shadowlength,1);
 		OnCommand=function(self)
-			self:queuecommand('MPResults');
+			self:queuecommand('Results');
 		end;
-		MPResultsCommand=function(self)
+		ResultsCommand=function(self)
+			local results = STATSMAN:GetCurStageStats();
+			local tableresults =  results:GetPlayerStageStats('PlayerNumber_P1');
+			local points = tableresults:GetScore();
+			local combo = tableresults:MaxCombo();
+		--	local calories = tableresults:GetCaloriesBurned();
+			local maxpoints = tableresults:GetCurMaxScore();
+			local grade = tableresults:GetGrade();
+			local released = tableresults:GetHoldNoteScores('HoldNoteScore_LetGo');
+			local held = tableresults:GetHoldNoteScores('HoldNoteScore_Held');
+			local percent = tableresults:GetPercentDancePoints()*100;
+			local flawless = tableresults:GetTapNoteScores('TapNoteScore_W1');
+			local perfects = tableresults:GetTapNoteScores('TapNoteScore_W2');
+			local greats = tableresults:GetTapNoteScores('TapNoteScore_W3');
+			local goods = tableresults:GetTapNoteScores('TapNoteScore_W4');
+			local bads = tableresults:GetTapNoteScores('TapNoteScore_W5');
+			local misses = tableresults:GetTapNoteScores('TapNoteScore_Miss');
+			local stringgrade = "X";
+			if grade == 'Grade_Tier01' then
+				stringgrade = "AAAA";
+			elseif grade == 'Grade_Tier02' then
+				stringgrade = "AAA";
+			elseif grade == 'Grade_Tier03' then
+				stringgrade = "AA";
+			elseif grade == 'Grade_Tier04' then
+				stringgrade = "A";
+			elseif grade == 'Grade_Tier05' then
+				stringgrade = "B";
+			elseif grade == 'Grade_Tier06' then
+				stringgrade = "C";
+			elseif grade == 'Grade_Tier07' then
+				stringgrade = "D";
+			elseif grade == 'Grade_Failed' then
+				stringgrade = "F";
+			else
+				--[ something went wrong...
+				stringgrade = "ERROR";
+			end;
+			self:zoom(0.75);
+			self:align(0,0.5);
+			self:diffuse(color(theme_color));
+			self:x(SCREEN_LEFT+8);
+			self:y(SCREEN_BOTTOM/2);
+			self:settext("Here are your results Player 1!\n\n\n\n\nGrade: " .. stringgrade .. "\nPercent: " .. tostring(round(percent,1)) .. "%\n\n\n\n\nFlawless: " .. tostring(flawless) .. "\nPerfects: " .. tostring(perfects) .. "\nGreats: " .. tostring(greats) .. "\nGoods: " .. tostring(goods) .. "\nBads: " .. tostring(bads) .. "\nMiss: " .. tostring(misses) .. "\nHeld: " .. tostring(held) .. "\nReleased: " .. tostring(released) .. "\nCombo: " .. tostring(combo) .. "\n\n\n\n\nScore: " .. tostring(points) .. "/" .. tostring(maxpoints))
+		end;
+	},
+	-- results MP text
+--	LoadFont("SpoOky")..{
+--		Text="Results MP";
+--		InitCommand=cmd(x,SCREEN_LEFT+8;y,SCREEN_BOTTOM/2;diffuse,color(theme_color);zoom,0.75;align,0,0.5;shadowlength,1);
+--		OnCommand=function(self)
+--			self:queuecommand('MPResults');
+--		end;
+--		MPResultsCommand=function(self)
 			-- this is messy right now... i have a lot to do with this screen...
 			-- these do not work currently. I will have to wait for this sorry...
 		--	local results = STATSMAN:GetCurStageStats();
@@ -85,17 +138,17 @@ local t = Def.ActorFrame{
 --				--[ something went wrong...
 --				stringgrade = "ERROR";
 --			end;
-			self:zoom(0.75);
-			self:align(0,0.5);
-			self:diffuse(color(theme_color));
-			self:x(SCREEN_LEFT+8);
-			self:y(SCREEN_BOTTOM/2);
+--			self:zoom(0.75);
+--			self:align(0,0.5);
+--			self:diffuse(color(theme_color));
+--			self:x(SCREEN_LEFT+8);
+--			self:y(SCREEN_BOTTOM/2);
 			-- this... doesn't work?
-			self:settext("Here are your results!\n\nScore: " .. "\n\nStepMania Online is very broken right now... I will come back to this screen...\n- SpoOkyMagician");
+--			self:settext("Here are your results!\n\nScore: " .. "\n\nStepMania Online is very broken right now... I will come back to this screen...\n- SpoOkyMagician");
 --			self:settext("Here are your results!\n\nScore: " .. tostring(points[1]) .. "," .. tostring(points[2]) .. "," .. tostring(points[3]) .. "," .. tostring(points[4]) tostring(points[5]) .. "," .. tostring(points[6]) .. "," .. tostring(points[7]) .. "," .. tostring(points[8]) .. "," .. tostring(points[9]) .. "," .. tostring(points[10]) .. "," .. tostring(points[11]) .. "," .. tostring(points[12]) .. "," .. tostring(points[13]) .. "," .. tostring(points[14]) .. "," .. tostring(points[15]) .. "," .. tostring(points[16]) .. "," .. tostring(points[17]) .. "," .. tostring(points[18]) .. "," .. tostring(points[19]) .. "," .. tostring(points[20]) .. "," .. tostring(points[21]) .. "," .. tostring(points[22]) .. "," .. tostring(points[23]) .. "," .. tostring(points[24]) .. "," .. tostring(points[25]) .. "," .. tostring(points[26]) .. "," .. tostring(points[27]) .. "," .. tostring(points[28]) .. "," .. tostring(points[29]) .. "," .. tostring(points[30]) .. "," .. tostring(points[31]) .. "," .. tostring(points[32]) );
 --			self:settext("Here are your results!\n\n\n\n\nGrade: " .. stringgrade .. "\nPercent: " .. tostring(round(percent,1)) .. "%\n\n\n\n\nFlawless: " .. tostring(flawless) .. "\nPerfects: " .. tostring(perfects) .. "\nGreats: " .. tostring(greats) .. "\nGoods: " .. tostring(goods) .. "\nBads: " .. tostring(bads) .. "\nMiss: " .. tostring(misses) .. "\nHeld: " .. tostring(held) .. "\nReleased: " .. tostring(released) .. "\nCombo: " .. tostring(combo) .. "\n\n\n\n\nScore: " .. tostring(points) .. "/" .. tostring(maxpoints));
-		end;
-	},
+--		end;
+--	},
 	-- GLOBAL play music
 	sub_song
 };
